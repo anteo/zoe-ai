eec50311c86df9ae249e946564093ac882e50b1d
Anton Argirov <anton.argirov@gmail.com>
2026-04-10 19:06:11 +0700

    Specify model via `ZOE_MODEL`

--- lib/ai/zoe.rb
+++ lib/ai/zoe.rb
@@ -1,8 +1,9 @@
 module AI
   class Zoe < BaseAgent
     chat_model ::Chat
+    model ENV["ZOE_MODEL"]
     inputs :additional_instructions
-    tools [Tools::Draw]
+    tools [ Tools::Draw ]
     instructions
   end
 end
