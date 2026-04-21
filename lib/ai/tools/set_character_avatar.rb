de191e54f8753a68d9a33b43159a457fbbe317f0
Anton Argirov <anton.argirov@gmail.com>
2026-04-21 15:15:35 +0700

    Update `SetCharacterAvatar` tool: support setting avatars for specific characters via `character_id` parameter.

--- lib/ai/tools/set_character_avatar.rb
+++ lib/ai/tools/set_character_avatar.rb
@@ -2,22 +2,32 @@ module AI
   module Tools
     class SetCharacterAvatar < Tool
       description <<~END
-        Add an avatar for a current user using an attachment from the current chat
+        Add an avatar for a character using an attachment from the current chat
         (use only if user explicitly said to set his avatar)"
       END
       params do
+        characters = ::Character.all.map { |c| "#{c.id} (#{c.name})" }.join(", ")
+
         integer :attachment_id,
                 description: "Blob ID of the attachment to use as the avatar (from the IDs listed in the message)",
                 required: true
+
+        string :character_id,
+               description: "ID of the character to add image to. Available: #{characters}",
+               enum: ::Character.pluck(:id).map(&:to_s),
+               required: true
       end
 
-      def execute(attachment_id:)
+      def execute(attachment_id:, character_id:)
+        character = current_user.characters.find_by(id: character_id)
+        fail! "Character with ID #{character_id} not found" unless character
+
         blob = chat.attachments_blobs.find_by(id: attachment_id)
         fail! "Attachment with ID #{attachment_id} not found" unless blob
 
-        current_character.avatar.attach(blob)
+        character.avatar.attach(blob)
 
-        "New avatar added for #{current_character.name}"
+        "New avatar added for #{character.name}"
       end
     end
   end
