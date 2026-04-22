ce2c23b2b48d594525d776d17daa2e6f6a2d0b47
Anton Argirov <anton.argirov@gmail.com>
2026-04-22 17:22:14 +0700

    Add optional `id` attribute support to `avatar_for` helpers and chat logo images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,12 +6,12 @@ module ApplicationHelper
                        .render(text).html_safe
   end
 
-  def avatar_for(url: nil, size: 32, initials: nil)
+  def avatar_for(url: nil, size: 32, initials: nil, id: nil)
     css = "rounded-full shrink-0 object-cover shadow-md"
     style = "width: #{size}px; height: #{size}px;"
 
     if url
-      image_tag(url, class: css, style:)
+      image_tag(url, class: css, style:, id:)
     else
       font_size = [ (size / 2.8).round, 12 ].max
       tag.span(
@@ -22,12 +22,12 @@ module ApplicationHelper
     end
   end
 
-  def avatar_for_user(user, size: 32)
-    avatar_for(url: user&.gravatar_url(size: size), initials: user&.initials, size:)
+  def avatar_for_user(user, size: 32, id: nil)
+    avatar_for(url: user&.gravatar_url(size: size), initials: user&.initials, size:, id:)
   end
 
-  def avatar_for_character(character, size: 32)
+  def avatar_for_character(character, size: 32, id: nil)
     url = url_for(character.avatar.variant(resize_to_limit: [ size ] * 2)) if character.avatar.attached?
-    avatar_for(url:, initials: character.name[0].upcase, size:)
+    avatar_for(url:, initials: character.name[0].upcase, size:, id:)
   end
 end
