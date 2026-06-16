52e85d7e1e5b2eedf85dc9e97d2dc50c6bb8a2f5
Anton Argirov <anton.argirov@gmail.com>
2026-06-16 16:04:05 +0700

    chore(ai): refactor ModelsPatch to use ActiveSupport::Concern

--- lib_static/patches/ruby_llm/models_patch.rb
+++ lib_static/patches/ruby_llm/models_patch.rb
@@ -1,21 +1,38 @@
 module Patches
   module RubyLLM
     module ModelsPatch
+      extend ActiveSupport::Concern
+
       FAKE_DEV_MODEL_ID = "fake-dev"
       FAKE_DEV_PROVIDER = "fake"
 
-      def read_from_json(file = ::RubyLLM.config.model_registry_file)
-        models = super
-        return models unless Rails.env.development?
+      class_methods do
+        def read_from_json(file = ::RubyLLM.config.model_registry_file)
+          models = super
+          return models unless Rails.env.development?
 
-        models + development_models(models)
-      end
+          models + development_models(models)
+        end
 
-      def load_existing_models
-        models = super
-        return models unless Rails.env.development?
+        def load_existing_models
+          models = super
+          return models unless Rails.env.development?
 
-        models + development_models(models)
+          models + development_models(models)
+        end
+
+        private
+
+        def development_models(existing_models)
+          return [] unless ::RubyLLM::Provider.resolve(:fake)
+          return [] if existing_models.any? { |model| fake_dev_model?(model) }
+
+          ::AI::Providers::Fake.new(::RubyLLM.config).list_models
+        end
+
+        def fake_dev_model?(model)
+          model.id == FAKE_DEV_MODEL_ID && model.provider == FAKE_DEV_PROVIDER
+        end
       end
 
       def save_to_json(file = ::RubyLLM.config.model_registry_file)
@@ -25,16 +42,7 @@ module Patches
 
       private
 
-      def development_models(existing_models)
-        return [] unless ::RubyLLM::Provider.resolve(:fake)
-        return [] if existing_models.any? { |model| fake_dev_model?(model) }
-
-        ::AI::Providers::Fake.new(::RubyLLM.config).list_models
-      end
-
-      def fake_dev_model?(model)
-        model.id == FAKE_DEV_MODEL_ID && model.provider == FAKE_DEV_PROVIDER
-      end
+      delegate :fake_dev_model?, to: :class, private: true
     end
   end
 end
